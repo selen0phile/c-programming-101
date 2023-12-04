ddfaee0a996951c8c22ac56b144d72c8c5321948
Jaber <jab3r0.0@gmail.com>
2023-12-04 16:13:31 +0600

    netlify deployed

--- generate_html.py
+++ generate_html.py
@@ -13,11 +13,12 @@ folder_path = './'
 files = get_all_files(folder_path)
 
 html = open('index.html','w')
-html.write('<body bgcolor="black">\n<center>\n')
+html.write('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"><body style="background-color:black !important;">\n<center>\n')
 for file in files:
     folder = file.split("\\")[1]
     name = file.split("\\")[2].replace(".png","")
-    html.write(f'<h3 style="color:white;font-family:consolas">{folder} / {name}</h3><img src="{file}" width="400px"/><br/><br/>\n')
+    c = file.replace(".png","")
+    html.write(f'<h3 style="color:white;font-family:consolas">{folder} / {name}</h3><a style="text-decoration:none;" href="{c}"><button class="btn btn-danger">Download</button></a><br/><br/><img src="{file}" width="400px"/><br/><br/>\n')
 
 html.write("</center>\n</body>\n")
 html.close()
